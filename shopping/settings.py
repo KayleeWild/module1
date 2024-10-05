7fcf432b96ad22ed629cd3c5eeeb35d57af83974
KayleeWild <crazykaylee7@byui.edu>
2024-10-04 20:43:29 -0600

    added cart app

--- shopping/settings.py
+++ shopping/settings.py
@@ -33,6 +33,7 @@ ALLOWED_HOSTS = []
 INSTALLED_APPS = [
     'pages.apps.PagesConfig',
     'items.apps.ItemsConfig',
+    'cart.apps.CartConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
